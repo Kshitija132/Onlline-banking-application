c1ee7eb4986e0ce176bfc89362979046a446fe03
Kshitija Talathi <141553169+Kshitija132@users.noreply.github.com>
2024-07-14 14:03:35 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,45 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+        <div class="header_main">
+		<div class="navBar">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php#aboutus">About Us</a></li>
+                <li><a href="index.php#contactus">Contact Us</a></li>
+				<li><a href="staff_login.php">Staff Login</a></li>
+			
+			</ul>
+		</div>
+	 <a href="index.php"><div class="logo-name">
+			<div class="logo">
+             <img class="logo_img" alt="Image" style="margin-right:0px;height:50px;width:50px;margin-left:10px" src="img/ch.jpg">
+			</div>
+			
+			<div class="name">
+				<h5 style="margin-right:150px;text-align:left;">SBI Online Banking System</h5></a><br>
+
+			</div>
+        </div>
+            
+            
+            <div class="dif_banking">
+			<div class="retail_banking">
+				<a href="#">Retail Banking</a>
+			</div>
+			<div class="corporate_banking">
+				<a href="#"> Corporate Banking</a>
+			</div>
+			<div class="international_banking">
+				<a href="#"> International Banking</a>
+			</div>
+			<div class="international_banking">
+				<a href="#"> Apply Mobile Banking</a>
+			</div>
+                
+            <div class="bank_servic">
+				<a href="#">Services</a>
+			</div>
+		</div>
+            
+           
+			
+	</div>
